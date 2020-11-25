a2b371962f64c49ec2da0eb7e35694685b77b6ba
Nataila <ccfive814@gmail.com>
2020-11-25 18:40:09 +0800

    user => account

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -8,8 +8,8 @@
 """
 
 from fastapi import APIRouter
-from api.v1.user import user
+from api.v1.account import account
 
 api_v1 = APIRouter()
 
-api_v1.include_router(user.router, tags=["用户管理"])
+api_v1.include_router(account.router, tags=["用户管理"])
